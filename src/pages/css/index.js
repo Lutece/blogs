9315a43dc75c869a42f74e88aa8e8be372a1b09b
Lutece <code1st@naver.com>
2018-01-07 04:02:06 +0900

    upload source

--- src/pages/css/index.js
+++ src/pages/css/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const CssPage = () => (
-  <div>
+  <div style={{ textAlign: 'center' }}>
     <h1>CSS3 글 목록</h1>
     <Link to="/css/flex">Flex</Link>
   </div>
