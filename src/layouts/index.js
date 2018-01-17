66f05bc031e6696a98cfae8452a2350181a9ce4b
Lutece <code1st@naver.com>
2018-01-18 03:23:33 +0900

    upload source

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,42 +3,42 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './font.scss';
+import './index.scss';
 
 const Header = () => (
-  <div
-    style={{
-      margin:'0 4em 1.45em 4em',
-      borderBottom: '1px solid #000',
-      textAlign: 'center'
-    }}
-  >
+  <header>
     <div
       style={{
         margin: '0 auto',
         maxWidth: 600,
-        padding: '1.45rem 1.0875rem'
+        padding: '1.45rem 1.0875rem',
+        textAlign: 'center'
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 className="header--title">
         <Link
           to="/"
-          style={{
-            color: '#000',
-            textDecoration: 'none'
-          }}
+          className="header--link"
         >
-          Lutece 블로그
+          읽고 다시 쓰는 블로그
         </Link>
       </h1>
+      <p>
+          개발과 관련된 개념들을 배우고 기록하는 블로그입니다.
+      </p>
+      <ul className="header--category">
+        <li>React</li>
+        <li>Webpack</li>
+        <li>Mobx</li>
+      </ul>
     </div>
-  </div>
+  </header>
 )
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="읽고 다시 쓰기"
       meta={[
         { name: 'description', content: 'Tech blog by Lutece' },
         { name: 'keywords', content: 'javascript,css,html,react,webpack,mobx,git,gulp' },
@@ -48,9 +48,10 @@ const TemplateWrapper = ({ children }) => (
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 600,
+        maxWidth: 900,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
+        textAlign: 'center'
       }}
     >
       {children()}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div style={{ margin: '3rem auto', maxWidth: 600 }}>
-    <h2>개발자 블로그입니다.</h2>
-    
+  <div style={{ margin: '3rem auto', maxWidth: 900 }}>
     <div>
-      <div style={{ marginBottom: '0.5em' }}>연재 중인 글 목록</div>
-      <ul>
+      <div style={{ marginBottom: '0.5em' }}>다음 목표: masonry을 이용하여 index 레이아웃 완성하기</div>
+      {/* <ul>
         <li>
           <Link to="/css">CSS3</Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   </div>
 )
