4308baed87a9af895148d4e6c41c9a1606d525a4
Lutece <code1st@naver.com>
2018-01-21 14:38:59 +0900

    upload source

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-96449606-2`,
       },
     },
     `gatsby-plugin-sass`
--- src/pages/google/googlee2b21f8e034d2cbe.md
+++
@@ -1,7 +0,0 @@
----
-path: "/googlee2b21f8e034d2cbe.html"
-date: "2018-01-20"
-title: "google"
----
-
-google-site-verification: googlee2b21f8e034d2cbe.html
